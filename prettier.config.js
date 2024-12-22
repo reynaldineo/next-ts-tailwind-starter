903bd984b740c68e34298adfeca180ff178376e1
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-12-23 01:09:31 +0700

    chore: add Prettier configuration and update using pnpm

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "all", // Add trailing commas wherever possible
+  semi: true, // Add semicolons at the end of statements
+  singleQuote: true, // Use single quotes instead of double quotes
+  jsxSingleQuote: true, // Use single quotes for JSX attributes
+  arrowParens: "always", // Always include parentheses around arrow function parameters
+  tabWidth: 2, // Set the number of spaces per indentation level
+  useTabs: false, // Use spaces instead of tabs
+  bracketSpacing: true, // Print spaces between brackets in object literals
+  endOfLine: "lf", // Ensure files use line feed (LF) endings
+};
